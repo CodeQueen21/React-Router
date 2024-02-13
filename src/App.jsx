1781ca585def5b1fe6fe03e50fa8b6c476ea1261
Raelinda Shoates <redfire809@gmail.com>
2024-02-12 19:34:49 -0500

    Added a home landing page and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import { StrictMode } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/Blue">blue</Link>
+          <Link to="/Red">red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </div>
